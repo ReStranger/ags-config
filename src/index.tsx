e70be855df1e93ccb81dd11bd5d0b930bcabe229
ReStranger <strengerplayr@gmail.com>
2024-12-05 13:09:21 +0300

    Refactoring
    - Move all file to src folder
    
    Signed-off-by: ReStranger <strengerplayr@gmail.com>

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { App, Astal } from "astal/gtk3";
+import styles from "./styles/index.scss";
+import Bar from "./widget/Bar";
+
+export const index: any = () => {
+  return App.start({
+    gtkTheme: "adw-gtk3",
+    iconTheme: "MoreWaita",
+    css: styles,
+    main() {
+      App.get_monitors().map(Bar);
+    },
+  });
+};
